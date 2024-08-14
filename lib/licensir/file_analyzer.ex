5491c3ac765e8b6dd3966cd4d0650f3435ae7e86
David Clark <david.clark.1043@gmail.com>
2024-08-14 17:17:00 -0500

    More BSD 2 updates

--- lib/licensir/file_analyzer.ex
+++ lib/licensir/file_analyzer.ex
@@ -6,7 +6,7 @@ defmodule Licensir.FileAnalyzer do
   @files [
     apache2: ["Apache2_text.txt", "Apache2_text.variant-2.txt", "Apache2_url.txt"],
     bsd_3_clause: ["BSD-3.txt", "BSD-3.variant-2.txt", "BSD-3.variant-3.txt", "BSD-3.variant-4.txt", "BSD-3.variant-5.txt"],
-    bsd_2_clause: ["BSD-2.txt", "BSD-2.variant-2.txt"],
+    bsd_2_clause: ["BSD-2.txt", "BSD-2.variant-2.txt", "BSD-2.variant-3.txt"],
     cc0: ["CC0-1.0.txt"],
     gpl_v2: ["GPLv2.txt"],
     gpl_v3: ["GPLv3.txt"],
